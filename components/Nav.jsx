eef61dc7823cfcb0afc91e4488ff110ce15a56a4
Eunice Yang <euniceyang21@gmail.com>
2023-12-03 15:44:11 -0600

    Build model and add database

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,16 +6,17 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders} from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn= true;
+  const {data: session} = useSession();
+
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
       setProviders(response);
     }
-    setProviders();
+    setUpProviders();
   },[])
 
   return (
@@ -31,9 +32,12 @@ const Nav = () => {
         <p className='logo_text'>Promptopia</p>
       </Link>
 
+      {/* {alert(session?.user)} */}
+      {/* {alert(providers)} */}
+
       {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href="/create-prompt" className='black_btn'>
               Create Post
@@ -71,7 +75,7 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex'>
             <Image
               src="/assets/images/logo.svg"
