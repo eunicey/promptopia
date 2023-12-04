a5c5ae4562f23be474771317b141416b86e80810
Eunice Yang <euniceyang21@gmail.com>
2023-12-04 16:15:16 -0600

    Add delete functionality

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -26,7 +26,22 @@ const MyProfile = () => {
   }
 
   const handleDelete = async(post)=> {
+    const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
 
+    if(hasConfirmed){
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: 'DELETE'
+        })
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id); // exclude deleted post
+
+        setPosts(filteredPosts);
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
   }
 
 
