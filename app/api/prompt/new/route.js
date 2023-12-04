ca6c070806470659cbdc72667113649c63145f73
Eunice Yang <euniceyang21@gmail.com>
2023-12-03 20:33:42 -0600

    Add prompt model, build API route

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,12 +1,22 @@
 import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
 
 export const POST = async (req) => {
   const {userId, prompt, tag} = await req.json();
 
   try {
-    await
+    await connectToDB(); // have to call this every time b/c it's a lamda function meaning it stops once it's done its job 
+    const newPrompt = new Prompt({
+      creator: userId,
+      tag,
+      prompt,
+    })
+
+    await newPrompt.save();
+    return new Response(JSON.stringify(newPrompt), { status: 201})
+
   } catch (error) {
-    
+    return new Response("Failed to create a new prompt", {status: 500})
   }
 }
 
